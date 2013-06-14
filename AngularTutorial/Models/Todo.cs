657eb7ca0add11108eeafa88505eba125a0b735d
LeoHP1 <y.leonardus@lamed.web.id>
2013-06-14 11:59:44 +0700

    Initial Commit

---
+++ AngularTutorial/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AngularTutorial.Models
+{
+    public class Todo
+    {
+        public int  Id { get; set; }
+        [MaxLength (800)]
+        public String Text { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int Priority { get; set; }
+    }
+}
